a98d96c3aecc6f59fe63173a3038f0167f3785e2
gavincode <gavingong123@gmai.com>
2015-04-02 17:42:04 +0800

    part2

---
+++ FileRelease.DAL/Project/ProjectDAL.cs
@@ -0,0 +1,26 @@
+﻿// ****************************************
+// FileName:ProjectDAL.cs
+// Description:
+// Tables:
+// Author:Gavin
+// Create Date:2015/3/30 11:08:15
+// Revision History:
+// ****************************************
+
+using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FileRelease.DAL
+{
+    using NHibernate;
+    using FileRelease.Model;
+
+    /// <summary>
+    /// Project数据访问
+    /// </summary>
+    public class ProjectDAL : BaseDAL
+    {
+    }
+}
---
+++ FileRelease.Model/MonitorInterceptor.cs
@@ -0,0 +1,30 @@
+﻿// ****************************************
+// FileName:Monitor.cs
+// Description:
+// Tables:
+// Author:Gavin
+// Create Date:2015/3/30 17:07:08
+// Revision History:
+// ****************************************
+
+using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FileRelease.Model
+{
+    using NHibernate;
+    using NHibernate.SqlCommand;
+    using Util.Log;
+
+    public class MonitorInterceptor : EmptyInterceptor
+    {
+        public override SqlString OnPrepareStatement(SqlString sql)
+        {
+            //将sql写入日志
+            LogUtil.Write(sql.ToString(), LogType.Debug);
+            return base.OnPrepareStatement(sql);
+        }
+    }
+}
